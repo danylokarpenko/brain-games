5b63e47021fb3788719c4769d17abdd0792938da
Danylo Karpenko <danilo_karpenko@ukr.net>
2019-06-12 17:34:18 +0300

    synx

--- dist/index.js
+++ dist/index.js
@@ -3,17 +3,17 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.whatIsYouName = void 0;
+exports.whatIsYourName = void 0;
 
 var _readlineSync = _interopRequireDefault(require("readline-sync"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const whatIsYouName = () => {
+const whatIsYourName = () => {
   const name = _readlineSync.default.question('May I have your name?');
 
   console.log(`Hello, ${name}!`);
 };
 
-exports.whatIsYouName = whatIsYouName;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6WyJ3aGF0SXNZb3VOYW1lIiwibmFtZSIsInJlYWRsaW5lU3luYyIsInF1ZXN0aW9uIiwiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6Ijs7Ozs7OztBQUFBOzs7O0FBQ08sTUFBTUEsYUFBYSxHQUFHLE1BQUs7QUFDaEMsUUFBTUMsSUFBSSxHQUFHQyxzQkFBYUMsUUFBYixDQUFzQix1QkFBdEIsQ0FBYjs7QUFDQUMsRUFBQUEsT0FBTyxDQUFDQyxHQUFSLENBQWEsVUFBU0osSUFBSyxHQUEzQjtBQUNELENBSE0iLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgcmVhZGxpbmVTeW5jIGZyb20gJ3JlYWRsaW5lLXN5bmMnO1xuZXhwb3J0IGNvbnN0IHdoYXRJc1lvdU5hbWUgPSAoKSA9PntcbiAgY29uc3QgbmFtZSA9IHJlYWRsaW5lU3luYy5xdWVzdGlvbignTWF5IEkgaGF2ZSB5b3VyIG5hbWU/Jyk7XG4gIGNvbnNvbGUubG9nKGBIZWxsbywgJHtuYW1lfSFgKTtcbn1cbiJdfQ==
+exports.whatIsYourName = whatIsYourName;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6WyJ3aGF0SXNZb3VyTmFtZSIsIm5hbWUiLCJyZWFkbGluZVN5bmMiLCJxdWVzdGlvbiIsImNvbnNvbGUiLCJsb2ciXSwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFBQTs7OztBQUNPLE1BQU1BLGNBQWMsR0FBRyxNQUFLO0FBQ2pDLFFBQU1DLElBQUksR0FBR0Msc0JBQWFDLFFBQWIsQ0FBc0IsdUJBQXRCLENBQWI7O0FBQ0FDLEVBQUFBLE9BQU8sQ0FBQ0MsR0FBUixDQUFhLFVBQVNKLElBQUssR0FBM0I7QUFDRCxDQUhNIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHJlYWRsaW5lU3luYyBmcm9tICdyZWFkbGluZS1zeW5jJztcbmV4cG9ydCBjb25zdCB3aGF0SXNZb3VyTmFtZSA9ICgpID0+e1xuICBjb25zdCBuYW1lID0gcmVhZGxpbmVTeW5jLnF1ZXN0aW9uKCdNYXkgSSBoYXZlIHlvdXIgbmFtZT8nKTtcbiAgY29uc29sZS5sb2coYEhlbGxvLCAke25hbWV9IWApO1xufVxuIl19
