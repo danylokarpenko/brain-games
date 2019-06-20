8e2a8479ab4415cf9bc49974a0fe7f03d25c25b7
Danylo Karpenko <danilo_karpenko@ukr.net>
2019-06-20 13:47:04 +0300

    solving synx problems

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import * as logic from '..';
-import brainEven from '../games/mini-brain-even';
+import brainEven from '../games/mini-game-even';
 
 const userName = logic.welcome('Answer "yes" if number even otherwise answer "no"');
 
