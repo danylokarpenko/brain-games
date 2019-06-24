c5ff36590a77aed8e694caa166327151138e4289
Danylo Karpenko <danilo_karpenko@ukr.net>
2019-06-25 00:23:17 +0300

    Deleted unnessessary comments in code

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,31 +1,5 @@
 import { getRandomInt, run } from '..';
 
-// const sum = (a, b) => {
-//   const question = b > 0 ? `${a} + ${b}` : `${a} - ${-b}`;
-//   const correctAnswer = a + b;
-//   return [question, correctAnswer];
-// };
-// const sub = (a, b) => {
-//   const question = b > 0 ? `${a} - ${b}` : `${a} + ${-b}`;
-//   const correctAnswer = a - b;
-//   return [question, correctAnswer];
-// };
-// const mul = (x, y) => {
-//   let a = x;
-//   let b = y;
-//   if (Math.abs(a) > 30 || Math.abs(b) > 20) {
-//     a = getRandomInt(-100, 100);
-//     b = getRandomInt(-10, 10);
-//   }
-//   const question = b > 0 ? `${a} * ${b}` : `${a} * (${b})`;
-//   const correctAnswer = a * b;
-//   return [question, correctAnswer];
-// };
-// const operation = [3];
-// operation[0] = sum;
-// operation[1] = sub;
-// operation[2] = mul;
-
 const operations = (operationIndex, a, b) => {
   switch (operationIndex) {
     case 1: {
