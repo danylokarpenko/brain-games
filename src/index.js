32f0ec3fbd47465f57a1f409be460710bcf6882a
Danylo Karpenko <danilo_karpenko@ukr.net>
2019-06-26 00:15:35 +0300

    Finall corrections

--- src/index.js
+++ src/index.js
@@ -6,14 +6,13 @@ const run = (gameDescription, generateRaund) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hi, ${userName}!\n`);
+
   for (let i = 0; i < raundsCount; i += 1) {
-    if (generateRaund === 0) break;
     const raundData = generateRaund();
     const question = raundData[0];
     const correctAnswer = raundData[1];
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-
     if (correctAnswer === userAnswer) console.log('Correct!');
     else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
